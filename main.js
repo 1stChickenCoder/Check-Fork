3a460d3256a1f4306a6e2f5b53312281aff3c2e3
Hoan Doan Duy <hoan03032001@gmail.com>
2022-09-27 23:21:49 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,69 @@
+const labels = ['Jan','Feb','Mar','Apl','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const inputMax = document.getElementById("inputText1");
+const inputMin = document.getElementById("inputText2");
+function getRanNumbers(max,min){
+    var a = [];
+    for (var i = 0; i<12; i++){
+        a[i] = Math.random() * (max - min) + min;  
+    }
+    return a;
+}
+const data = {
+    labels: labels,
+    datasets :[
+        {
+            label: 'Nhiệt độ',
+            backgroundColor: "aqua",
+            borderColor:"black",
+            data:  getRanNumbers(50,0),
+            tension: 0.2,
+        },
+        {
+            label: 'Độ ẩm',
+            backgroundColor: "brown",
+            borderColor:"black",
+            data:  getRanNumbers(50,0),
+            tension: 0.2,
+        },
+        {
+            label: 'Ánh sáng',
+            backgroundColor: "yellow",
+            borderColor:"black",
+            data:  getRanNumbers(50,0),
+            tension: 0.2,
+        }
+    ]
+}
+const config = {
+    type: 'line',
+    data: data,
+}
+const canvas = document.getElementById('canvas');
+const chart = new Chart(canvas, config);
+
+function addNum1(event){
+    if (inputMin.value !== "" && inputMax.value !== "") {
+        var arr1= getRanNumbers(Number(inputMax.value),Number(inputMin.value));
+        chart.config.data.datasets[0].data = arr1;
+        chart.update();
+    }
+}
+function addNum2(event){
+    if (inputMin.value !== "" && inputMax.value !== "") {
+        var arr2= getRanNumbers(Number(inputMax.value),Number(inputMin.value));
+        chart.config.data.datasets[0].data = arr2;
+        chart.update();
+    } 
+}
+
+const button = document.querySelector('.button1');
+button.addEventListener('click',changeColor);
+function changeColor(){
+    const background = document.querySelector('.button');
+    const arrayColor = ['red','yellow','pink','gray','black','orange','blue'];
+    let random = arrayColor[randomColor(arrayColor)];
+    background.style.backgroundColor = random;
+}
+function randomColor(array){
+    return Math.floor(Math.random()*array.length);
+}
